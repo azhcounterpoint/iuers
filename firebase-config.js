b6e6ca885b9dbd859863c899147dad7d17b586a7
azhcounterpoint <azhcounterpoint@gmail.com>
2025-07-07 12:51:18 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAV0ZgEYMupAu9b8xZRuNV00HUouElztrw",
   authDomain: "iupaers.firebaseapp.com",
+  databaseURL: "https://iupaers-default-rtdb.firebaseio.com",
   projectId: "iupaers",
   storageBucket: "iupaers.firebasestorage.app",
   messagingSenderId: "988099496172",
-  appId: "1:988099496172:web:49d52b7d63593736257dc9"
+  appId: "1:988099496172:web:2a8a39ebe6c7d2ac257dc9"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
